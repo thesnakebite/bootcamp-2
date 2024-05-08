2a778433102ff7866fb4e4675515af6232448123
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2024-05-08 07:45:35 +0200

    First commit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// personal-site.com => home
+// personal-site.com/blog => blog
+// personal-site.com/nosotros => about
+//personal-site.com/contacto => contact
+
+Route::view('/', 'home')->name('home');
+Route::view('/blog', 'blog')->name('blog');
+Route::view('/nosotros', 'about')->name('about');
+Route::view('/contacto', 'contact')->name('contact');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
