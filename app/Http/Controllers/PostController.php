85d5d548573a50bb61a37542e73c6de16ee56f7c
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2024-05-14 10:41:46 +0200

    Reusable form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -48,7 +48,9 @@ class PostController extends Controller
 
     public function create()
     {
-        return view('posts.create');
+        return view('posts.create', [
+            'post' => new Post
+        ]);
     }
 
     public function store(Request $request)
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,17 +11,7 @@
         >
 
             @csrf
-            <label for="title" class=" font-worksansmedium">{{ __('Title') }}</label>
-            <input name="title" id="title" type="text" class="border-2 border-slate-400 p-2 mb-4" value="{{ old('title') }}">
-            @error('title')
-                <small class="bg-red-200 border-red-500 border-2 rounded p-2 mb-2 text-red-500">{{ $message }}</small>
-            @enderror
-
-            <label for="body" class="font-worksansmedium">{{ __('Body') }}</label>
-            <textarea name="body" id="body" cols="30" rows="10" class="border-2 border-slate-400 mb-4">{{old('body')}}</textarea>
-            @error('body')
-                <small class="bg-red-200 border-red-500 border-2 rounded p-2 mb-2 text-red-500">{{ $message }}</small>
-            @enderror
+           @include('posts.form-field')
 
             <button class="w-36 bg-white text-black hover:bg-slate-900 hover:text-white font-worksansmedium border-2 border-slate-400 p-2 rounded-md transition-all ease-out">
                 {{ __('Post create') }}
